8f4b031d53c4d2ef46fc607a337cab448f288f52
KostiantynZ <integrity1@mail.ru>
2018-06-11 09:47:53 +0300

    0) Basic Settings:

---
+++ app/Company.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    protected $fillable = ['name', 'email', 'logo', 'website'];
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+}
